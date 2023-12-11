0426d1b30ba6bf27ac66759a1134a7b7cde80d25
yiwang <47427893+subarashikihibi@users.noreply.github.com>
2023-12-11 16:29:49 +0800

    Revert "failhighcnt++ if large fail high"
    This reverts commit 58018d30594136572490af8c6bc8cc254402ea21.

--- Rapfi/search/ab/search.cpp
+++ Rapfi/search/ab/search.cpp
@@ -543,11 +543,8 @@ void aspirationSearch(Rule rule, Board &board, SearchStack *ss, Value prevValue,
             failHighCnt = 0;
         }
         else if (value >= beta) {
-            if(value >= beta + 250)
-                failHighCnt++;
             beta = std::min(value + delta, VALUE_INFINITE);
             failHighCnt++;
-
         }
         else
             break;
